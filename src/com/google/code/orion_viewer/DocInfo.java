e4ef3c2bf84af6b7a020176cbf4b0ca751f83c40
mikhael.bogdanov <mikhael.bogdanov@gmail.com>
2011-12-14 23:14:38 +0400

    path rendering on e-ink screen & new document title extraction

---
+++ src/com/google/code/orion_viewer/DocInfo.java
@@ -0,0 +1,27 @@
+package com.google.code.orion_viewer;
+
+/**
+ * User: mike
+ * Date: 14.12.11
+ * Time: 16:37
+ */
+public class DocInfo {
+
+    public int pageCount;
+
+    public String title;
+
+    public String fileName;
+
+    public DocInfo() {
+
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
