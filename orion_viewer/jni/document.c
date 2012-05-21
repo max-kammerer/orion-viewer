2d1bd01aad84af39260d15c1731e6eddc273da5a
mikhael.bogdanov <mikhael.bogdanov@gmail.com>
2012-05-21 10:38:19 +0400

    clean

--- orion_viewer/jni/document.c
+++ orion_viewer/jni/document.c
@@ -16,25 +16,20 @@ unsigned int contrast = 100;
 JNIEXPORT void
 JNICALL Java_universe_constellation_orion_viewer_AbstractDocumentWrapper_setContrast(JNIEnv * env, jobject thiz, jint contrast1)
 {
-   LOGI("Set contrast : %i", contrast1);		
-    contrast = contrast1;
-    float kgamma = 100.0f / contrast1;
-int i;
-for (i = 0; i < 256; i++) 
-    {
-        orion_gamma[i] = pow(i / 255.0f, kgamma) * 255;
- 	}
-}
-
-
-void orion_updateContrast(unsigned char * data , int size) {
-if (contrast != DEFAULT_CONTRAST) {
-   LOGI("Update gamma : %i", size);		
-	int i;
-    for (i = 0; i < size; i++) 
-    {
-        data[i] = orion_gamma[data[i]];
+    LOGI("Set contrast : %i",contrast1);
+    contrast=contrast1;
+    float kgamma=100.0f/contrast1;
+    int i;
+    for(i=0;i<256;i++) {
+        orion_gamma[i]=pow(i/255.0f,kgamma)*255;
     }
 }
 
+void orion_updateContrast(unsigned char * data , int size) {
+    if (contrast != DEFAULT_CONTRAST) {
+    LOGI("Update gamma : %i", size);
+	int i;
+    for (i = 0; i < size; i++) {
+        data[i] = orion_gamma[data[i]];
+    }
 }
