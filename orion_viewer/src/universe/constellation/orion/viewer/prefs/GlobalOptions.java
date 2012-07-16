5a7d4fb6d3a44980ffac33c1c62197183b591fed
mikhael.bogdanov <mikhael.bogdanov@gmail.com>
2012-07-16 10:44:28 +0400

    CONSTANT value fix

--- orion_viewer/src/universe/constellation/orion/viewer/prefs/GlobalOptions.java
+++ orion_viewer/src/universe/constellation/orion/viewer/prefs/GlobalOptions.java
@@ -64,7 +64,7 @@ public class GlobalOptions implements Serializable {
     public final static String LONG_CROP_VALUE = "LONG_CROP_VALUE";
     public final static String SCREEN_OVERLAPPING_HORIZONTAL = "SCREEN_OVERLAPPING_HORIZONTAL";
     public final static String SCREEN_OVERLAPPING_VERTICAL = "SCREEN_OVERLAPPING_VERTICAL";
-    public final static String DEBUG = "SCREEN_OVERLAPPING_VERTICAL";
+    public final static String DEBUG = "DEBUG";
 
     public final static String BRIGHTNESS = "BRIGHTNESS";
 
