c3551f6d8cf692c4bd842378e39f542240d2358b
mikhael.bogdanov <mikhael.bogdanov@gmail.com>
2013-03-02 15:59:19 +0400

    Remove generated folders

--- orion-viewer/gen/universe/constellation/orion/viewer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package universe.constellation.orion.viewer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
