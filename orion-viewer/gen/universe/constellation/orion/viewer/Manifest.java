6139162307f167506ac75b73fe7c5f5f27826ce8
mikhael.bogdanov <mikhael.bogdanov@gmail.com>
2013-03-02 16:05:25 +0400

    Remove generated folders

--- orion-viewer/gen/universe/constellation/orion/viewer/Manifest.java
+++
@@ -1,5 +0,0 @@
-package universe.constellation.orion.viewer;
-
-/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
-public final class Manifest {
-}
--- orion-viewer/gen/universe/constellation/orion/viewer/R.java
+++
@@ -1,5 +0,0 @@
-package universe.constellation.orion.viewer;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
