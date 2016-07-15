2cca9a97e0d190f67fee73d356734da3fa496221
max.kammerer <max.kammerer@yahoo.com>
2016-07-15 15:53:01 +0300

    OptionActions code format

--- orion-viewer/src/main/java/universe/constellation/orion/viewer/OptionActions.java
+++ orion-viewer/src/main/java/universe/constellation/orion/viewer/OptionActions.java
@@ -44,38 +44,38 @@ public enum OptionActions {
     },
 
     SHOW_ACTION_BAR("SHOW_ACTION_BAR") {
-            public void doAction(OrionViewerActivity activity, boolean oldValue, boolean newValue) {
-                Toolbar toolbar = activity.getToolbar();
-                ViewGroup.LayoutParams layoutParams = toolbar.getLayoutParams();
-                if (newValue) {
-                    layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-                } else {
-                    layoutParams.height = 0;
-                }
-                toolbar.setLayoutParams(layoutParams);
-                Menu menu = toolbar.getMenu();
-                menu.clear();
-                activity.onCreateOptionsMenu(menu);
-                if (!newValue) {
-                    for (int i = 0; i < menu.size(); i++) {
-                        SupportMenuItem item = (SupportMenuItem) menu.getItem(i);
-                        item.setShowAsAction(SupportMenuItem.SHOW_AS_ACTION_NEVER);
-                    }
+        public void doAction(OrionViewerActivity activity, boolean oldValue, boolean newValue) {
+            Toolbar toolbar = activity.getToolbar();
+            ViewGroup.LayoutParams layoutParams = toolbar.getLayoutParams();
+            if (newValue) {
+                layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            } else {
+                layoutParams.height = 0;
+            }
+            toolbar.setLayoutParams(layoutParams);
+            Menu menu = toolbar.getMenu();
+            menu.clear();
+            activity.onCreateOptionsMenu(menu);
+            if (!newValue) {
+                for (int i = 0; i < menu.size(); i++) {
+                    SupportMenuItem item = (SupportMenuItem) menu.getItem(i);
+                    item.setShowAsAction(SupportMenuItem.SHOW_AS_ACTION_NEVER);
                 }
             }
-        },
+        }
+    },
 
     SHOW_STATUS_BAR("SHOW_ACTION_BAR") {
-         public void doAction(OrionViewerActivity activity, boolean oldValue, boolean newValue) {
-             activity.getStatusBarHelper().setShowStatusBar(newValue);
-         }
-     },
+        public void doAction(OrionViewerActivity activity, boolean oldValue, boolean newValue) {
+            activity.getStatusBarHelper().setShowStatusBar(newValue);
+        }
+    },
 
     SHOW_OFFSET_ON_STATUS_BAR("SHOW_OFFSET_ON_STATUS_BAR") {
-         public void doAction(OrionViewerActivity activity, boolean oldValue, boolean newValue) {
-             activity.getStatusBarHelper().setShowOffset(newValue);
-         }
-     },
+        public void doAction(OrionViewerActivity activity, boolean oldValue, boolean newValue) {
+            activity.getStatusBarHelper().setShowOffset(newValue);
+        }
+    },
 
     SCREEN_OVERLAPPING_HORIZONTAL("SCREEN_OVERLAPPING_HORIZONTAL") {
         public void doAction(OrionViewerActivity activity, int hor, int ver) {
@@ -148,10 +148,7 @@ public enum OptionActions {
 
     public void doAction(OrionViewerActivity activity, String oldValue, String newValue) {
 
-        }
-
-
-
+    }
 
     public String getKey() {
         return key;
