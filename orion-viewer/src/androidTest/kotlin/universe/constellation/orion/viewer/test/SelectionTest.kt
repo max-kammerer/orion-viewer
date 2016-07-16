d1cfd4fb82626d1d433e3c1a64e2c7911ce37504
max.kammerer <max.kammerer@yahoo.com>
2016-07-16 19:25:46 +0300

    Updated text selection test

--- orion-viewer/src/androidTest/kotlin/universe/constellation/orion/viewer/test/SelectionTest.kt
+++ orion-viewer/src/androidTest/kotlin/universe/constellation/orion/viewer/test/SelectionTest.kt
@@ -1,6 +1,7 @@
 package universe.constellation.orion.viewer.test
 
 import junit.framework.Assert
+import universe.constellation.orion.viewer.selection.SelectionAutomata
 import universe.constellation.orion.viewer.test.framework.BaseTest
 import universe.constellation.orion.viewer.test.framework.TestUtil
 
@@ -12,16 +13,32 @@ class SelectionTest() : BaseTest() {
 
     fun testSicp() {
         val book = openTestBook(TestUtil.SICP)
-        val text = book.getText(11/*zero based*/, 242, 172, 140, 8)
+        val text = book.getText(11/*zero based*/, 242, 172, 140, 8, false)
 
-        Assert.assertEquals(text, "These programs")
+        Assert.assertEquals("These programs", text)
+    }
+
+    fun testSicpSingleWord() {
+        val book = openTestBook(TestUtil.SICP)
+        val selectionRect = SelectionAutomata.getSelectionRectangle(250, 176, 0, 0, true)
+        val text = book.getText(11/*zero based*/, selectionRect.left, selectionRect.top, selectionRect.width(), selectionRect.height(), true)
+
+        Assert.assertEquals("These", text)
     }
 
     fun testAlice() {
         val book = openTestBook(TestUtil.ALICE)
-        val text = book.getText(5/*zero based*/, 1288, 517, 115, 50)
+        val text = book.getText(5/*zero based*/, 1288, 517, 115, 50, false)
 
-        Assert.assertEquals(text, "tunnel ")
+        Assert.assertEquals("tunnel ", text)
+    }
+
+    fun testAliceSingleWord() {
+        val book = openTestBook(TestUtil.ALICE)
+        val selectionRect = SelectionAutomata.getSelectionRectangle(518, 556, 0, 0, true)
+        val text = book.getText(5/*zero based*/, selectionRect.left, selectionRect.top, selectionRect.width(), selectionRect.height(), true)
+
+        Assert.assertEquals("The ", text)
     }
 
 }
