dc5a9dcd0e249911d925d4cfbc14cefac0f6153a
Michael Bogdanov <max.kammerer@yahoo.com>
2017-05-27 09:34:59 +0200

    Switch ndk and sdk compilation target to android 14, update dependencies

--- orion-viewer/jni/djvu/android/jni/Application.mk
+++ orion-viewer/jni/djvu/android/jni/Application.mk
@@ -1,7 +1,7 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := armeabi armeabi-v7a x86 mips
-APP_PLATFORM=android-8
-#armeabi 
+APP_ABI := armeabi-v7a x86 mips
+APP_PLATFORM=android-14
+#armeabi
 #armeabi-v7a mips x86
 APP_OPTIM := release
 LOCAL_ARM_MODE := arm
