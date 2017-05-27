c0486f704e10378cb0f87e6081ab8ad4d73156d9
Michael Bogdanov <max.kammerer@yahoo.com>
2017-05-27 09:34:59 +0200

    Switch ndk compilation target to android 9

--- orion-viewer/jni/djvu/android/jni/Application.mk
+++ orion-viewer/jni/djvu/android/jni/Application.mk
@@ -1,6 +1,6 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
 APP_ABI := armeabi armeabi-v7a x86 mips
-APP_PLATFORM=android-8
+APP_PLATFORM=android-9
 #armeabi 
 #armeabi-v7a mips x86
 APP_OPTIM := release
