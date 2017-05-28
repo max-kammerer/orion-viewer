e375628230bd87c80f346f4f7e583385c30596db
Michael Bogdanov <max.kammerer@yahoo.com>
2017-05-28 10:45:02 +0200

    Convert LayoutStrategy to Kotlin

---
+++ orion-viewer/src/main/java/universe/constellation/orion/viewer/LayoutStrategy.kt
@@ -0,0 +1,68 @@
+/*
+ * Orion Viewer - pdf, djvu, xps and cbz file viewer for android devices
+ *
+ * Copyright (C) 2011-2013  Michael Bogdanov & Co
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package universe.constellation.orion.viewer
+
+import android.graphics.Point
+import universe.constellation.orion.viewer.prefs.GlobalOptions
+
+interface LayoutStrategy {
+
+    val zoom: Int
+
+    val margins: CropMargins
+
+    val rotation: Int
+
+    val layout: Int
+
+    val walkOrder: String
+
+    val walker: PageWalker
+
+    fun nextPage(pos: LayoutPosition)
+
+    fun prevPage(pos: LayoutPosition)
+
+    fun reset(pos: LayoutPosition, pageNumber: Int)
+
+    fun reset(pos: LayoutPosition, pageNumber: Int, forward: Boolean)
+
+    fun changeRotation(rotation: Int): Boolean
+
+    fun changeOverlapping(horizontal: Int, vertical: Int): Boolean
+
+    fun reset(info: LayoutPosition, forward: Boolean, pageInfo: PageInfo, cropMode: Int, zoom: Int, doCentering: Boolean)
+
+    fun changeZoom(zoom: Int): Boolean
+
+    fun changeCropMargins(cropMargins: CropMargins): Boolean
+
+    fun init(info: LastPageInfo, options: GlobalOptions)
+
+    fun serialize(info: LastPageInfo)
+
+    fun convertToPoint(pos: LayoutPosition): Point
+
+    fun changeNavigation(walkOrder: String): Boolean
+
+    fun changePageLayout(navigation: Int): Boolean
+
+    fun setDimension(width: Int, height: Int)
+}
