4795e9a5006f592ad8cad1fefc7ea644d9b25220
Michael Bogdanov <max.kammerer@yahoo.com>
2017-07-08 15:22:58 +0200

    Convert DjvuDocument to Kotlin

--- orion-viewer/src/main/java/universe/constellation/orion/viewer/document/DocumentWithCaching.kt
+++ orion-viewer/src/main/java/universe/constellation/orion/viewer/document/DocumentWithCaching.kt
@@ -129,14 +129,6 @@ class DocumentWithCaching(val doc: Document) : Document by doc {
     override fun hasCalculatedPageInfo(pageNumber: Int): Boolean = cache[pageNumber] != null
 }
 
-inline fun <R> timing(message: String, l: () -> R): R {
-    Common.d("Starting task '$message'...")
-    val start = System.currentTimeMillis()
-    return l().also {
-        Common.d("Task '$message' is finished in ${System.currentTimeMillis() - start} ms")
-    }
-}
-
 abstract class Image(val width: Int, val height: Int) {
 
     abstract operator fun get(h: Int, w: Int): Int
