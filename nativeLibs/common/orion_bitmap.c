3f26a85db7a0e5b47c33f93a46bf699cc2768679
Mike Bogdanov <mikhael.bogdanov@gmail.com>
2019-01-04 16:42:37 +0100

    Fix warnings

--- nativeLibs/common/orion_bitmap.c
+++ nativeLibs/common/orion_bitmap.c
@@ -11,12 +11,12 @@
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
 
-unsigned char orion_gamma [256];
+unsigned char orion_gamma[256];
 unsigned const int DEFAULT_CONTRAST = 100;
 unsigned int contrast = 100;
 unsigned int threshold = 255;
 
-void orion_setContrast(JNIEnv * env, jobject thiz, jint contrast1);
+void orion_setContrast(JNIEnv *env, jobject thiz, jint contrast1);
 
 #ifdef ORION_PDF
 JNIEXPORT void
@@ -29,40 +29,38 @@ JNICALL Java_com_artifex_mupdfdemo_MuPDFCore_setContrast(JNIEnv * env, jobject t
 JNIEXPORT void
 JNICALL Java_com_artifex_mupdfdemo_MuPDFCore_setThreshold(JNIEnv * env, jobject thiz, jint threshold1)
 {
-	threshold = threshold1;
+    threshold = threshold1;
 }
 
 #else
 
 JNIEXPORT void
-JNICALL Java_universe_constellation_orion_viewer_djvu_DjvuDocument_setContrast(JNIEnv * env, jobject thiz, jint contrast1)
-{
+JNICALL Java_universe_constellation_orion_viewer_djvu_DjvuDocument_setContrast(JNIEnv *env, jobject thiz, jint contrast1) {
     orion_setContrast(env, thiz, contrast1);
 }
 
 JNIEXPORT void
-JNICALL Java_universe_constellation_orion_viewer_djvu_DjvuDocument_setThreshold(JNIEnv * env, jobject thiz, jint threshold1)
-{
-    threshold = threshold1;
+JNICALL Java_universe_constellation_orion_viewer_djvu_DjvuDocument_setThreshold(JNIEnv *env, jobject thiz, jint threshold1) {
+    threshold = (unsigned int) threshold1;
 }
 
 #endif
 
 void orion_setContrast(JNIEnv *env, jobject thiz, jint contrast1) {
     LOGI("Set contrast : %i", contrast1);
-    contrast = contrast1;
+    contrast = (unsigned int) contrast1;
     float kgamma = contrast1 / 100.0f;
     int i;
     for (i = 0; i < 256; i++) {
-        orion_gamma[i] = pow(i / 255.0f, kgamma) * 255;
+        orion_gamma[i] = (unsigned char) (pow(i / 255.0f, kgamma) * 255);
     }
 }
 
 
-void orion_updateContrast(unsigned char * data , int size) {
+void orion_updateContrast(unsigned char *data, int size) {
     if (contrast != DEFAULT_CONTRAST) {
-       LOGI("Update gamma : %i", size);
-	    int i;
+        LOGI("Update gamma : %i", size);
+        int i;
         for (i = 0; i < size; i++) {
             data[i] = orion_gamma[data[i]];
         }
@@ -71,9 +69,9 @@ void orion_updateContrast(unsigned char * data , int size) {
     if (threshold > 0 && threshold < 255) {
         int i;
         for (i = 0; i < size; i++) {
-        	if (data[i] > threshold) {
-    	        data[i] = 255;
-	        }
+            if (data[i] > threshold) {
+                data[i] = 255;
+            }
         }
     }
 }
