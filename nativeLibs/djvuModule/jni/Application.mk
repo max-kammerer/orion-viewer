c0b7706efa40922e34163c919f9b63ce691608c4
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-10-31 13:24:16 +0100

    Disble arm5 and mips abi (cause of NDK restrictions)
      Seems they could be easily supported with NDK 16

--- nativeLibs/djvuModule/jni/Application.mk
+++ nativeLibs/djvuModule/jni/Application.mk
@@ -1,8 +1,7 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := armeabi armeabi-v7a x86 mips
-APP_PLATFORM=android-14
-#armeabi
-#armeabi-v7a mips x86
+#armeabi mips
+APP_ABI := armeabi-v7a x86
+APP_PLATFORM=android-16
 APP_OPTIM := release
 APP_STL := system
 APP_CPPFLAGS += -fexceptions
--- nativeLibs/mupdfModule/jni/Application.mk
+++ nativeLibs/mupdfModule/jni/Application.mk
@@ -8,8 +8,9 @@
 
 # Version X+1: armeabi-v7a (Much faster due to the availability of hardware
 # FP, but cannot be run in the emulator).
-APP_PLATFORM=android-14
-APP_ABI := armeabi armeabi-v7a x86 mips
+APP_PLATFORM=android-16
+APP_ABI := armeabi-v7a x86
+#APP_ABI := armeabi armeabi-v7a x86 mips
 
 # Version X+2: x86 (Requires android-9, so a change needs to be made in
 # AndroidManifest.xml too)
