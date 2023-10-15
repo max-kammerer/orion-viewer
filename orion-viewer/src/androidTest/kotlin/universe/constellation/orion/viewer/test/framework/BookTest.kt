098bffeb579b3cb5cf0e7eb6eda24c4693950634
Michael Bogdanov <mikhael.bogdanov@gmail.com>
2023-10-15 14:48:53 +0200

    Added test for tap zone start screen

--- orion-viewer/src/androidTest/kotlin/universe/constellation/orion/viewer/test/framework/BookTest.kt
+++ orion-viewer/src/androidTest/kotlin/universe/constellation/orion/viewer/test/framework/BookTest.kt
@@ -1,15 +1,19 @@
 package universe.constellation.orion.viewer.test.framework
 
+import android.content.Intent
 import android.graphics.Point
+import android.net.Uri
+import android.os.Bundle
 import org.junit.After
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
+import universe.constellation.orion.viewer.BuildConfig
+import universe.constellation.orion.viewer.OrionViewerActivity
 
 @RunWith(Parameterized::class)
 abstract class BookTest(path: String) : BaseTest() {
 
     protected val document by lazy {  openTestBook(path) }
-
     @After
     fun close() {
         document.destroy()
@@ -26,5 +30,17 @@ enum class BookDescription(
 ) {
     SICP(TestUtil.SICP, 762, "", 15, 139, Point(662, 885)),
     ALICE(TestUtil.ALICE, 77, null, 0,  pageSize = Point(2481, 3508)),
-    DJVU_SPEC(TestUtil.DJVU_SPEC, 71, null, 1, 100, Point(2539, 3295))
+    DJVU_SPEC(TestUtil.DJVU_SPEC, 71, null, 1, 100, Point(2539, 3295));
+
+    fun toOpenIntent(): Intent {
+        val path = extractFileFromTestData(path)
+        return Intent(Intent.ACTION_VIEW).apply {
+            setClassName(
+                BuildConfig.APPLICATION_ID,
+                OrionViewerActivity::class.qualifiedName!!
+            )
+            data = Uri.fromFile(path)
+            addCategory(Intent.CATEGORY_DEFAULT)
+        }
+    }
 }
