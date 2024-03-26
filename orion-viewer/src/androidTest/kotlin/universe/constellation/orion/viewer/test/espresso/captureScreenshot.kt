bfc6fbd91ffe039a91c83594b1b0b18ee01fcfae
Michael Bogdanov <mikhael.bogdanov@gmail.com>
2024-03-26 07:51:54 +0100

    Small refactorings in base test utils

--- orion-viewer/src/androidTest/kotlin/universe/constellation/orion/viewer/test/espresso/captureScreenshot.kt
+++ orion-viewer/src/androidTest/kotlin/universe/constellation/orion/viewer/test/espresso/captureScreenshot.kt
@@ -7,12 +7,16 @@ import universe.constellation.orion.viewer.test.framework.dumpBitmap
 import java.text.SimpleDateFormat
 import java.util.Date
 
-private val simpleDateFormat: SimpleDateFormat = SimpleDateFormat("yyMMdd_HH_mm")
+private val simpleDateFormat: SimpleDateFormat = SimpleDateFormat("yyMMdd_HH_mm_ss")
 
 class ScreenshotTakingRule : TestWatcher() {
 
     override fun failed(e: Throwable?, description: Description) {
         dump(description.methodName.take(15))
+    }
+
+    fun takeScreenshot(methodName: String) {
+        dump("_" + methodName.take(15))
     }
 
     companion object {
